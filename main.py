e6a761976cac59b757c566d6391701cd67ee42d1
Bor <boristeryan10@gmail.com>
2026-06-06 23:55:52 +0400

    drone class

---
+++ main.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Drone:
+  def __init__(self):
+    self.x = 0
+    self.y = 0
+    self.x_dest = 100
+    self.y_dest = 100
+    self.v = 200
+    self.v_step = 5
+    self.full_battery = 100
+    self.battery_drop = 1
+
+  def move(self):
+    dx = self.x_dest - self.x
+    dy = self.y_dest - self.y
+    distance = np.sqrt(dx**2 + dy**2)
+
+    if distance > self.v_step:
+      self.x += self.v_step * (dx / distance)
+      self.y += self.v_step * (dy / distance)
+      self.full_battery -= self.battery_drop
+    else:
+      self.x = self.x_dest
+      self.y = self.y_dest
+      print("Drone achieved the aim!")
+
+
